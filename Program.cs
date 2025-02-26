c038ce7262e6547e50668dd0f76d2869da4d6c2b
hp <hp@DESKTOP-JJOR793>
2025-02-26 09:08:12 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactsBusinessLayer;
+namespace ContactsConsolApp2
+{
+    internal class Program
+    {
+
+        static void SetFinde(int ID)
+        {
+
+            clsContact contact1 = clsContact.Find(ID);
+
+            if (contact1 != null)
+            {
+
+                Console.WriteLine(contact1._FirstName + " " + contact1.LastName) ;
+                Console.WriteLine(contact1.Email);
+                Console.WriteLine(contact1.Phone);
+                Console.WriteLine(contact1.Address);
+                Console.WriteLine(contact1.DateOfBirth);
+                Console.WriteLine(contact1.CountryID);
+                Console.WriteLine(contact1.ImagePath);
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+        static void Main(string[] args)
+        {
+            SetFinde(1);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
